a22d971bc51176f82b8eeb0400c8a11847a03b83
kiwilss <kiwilss@163.com>
2021-02-19 17:08:33 +0800

    Initial commit

---
+++ version/src/main/java/com/kiwilss/version/BuildConfig.kt
@@ -0,0 +1,12 @@
+package com.kiwilss.version
+
+/**
+ *@FileName: BuildConfig
+ *@author : Lss kiwilss
+ * @e-mail : kiwilss@163.com
+ * @time   : 2021/2/19
+ * @desc   : {DESCRIPTION}
+ */
+object BuildConfig {
+
+}
---
+++ version/src/main/java/com/kiwilss/version/DependencyVersionPlugin.kt
@@ -0,0 +1,10 @@
+package com.kiwilss.version
+
+import org.apache.tools.ant.Project
+import org.gradle.api.Plugin
+
+class DependencyVersionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
